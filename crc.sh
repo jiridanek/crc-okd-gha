db15cca0d145cfed236f9cb2644a7dddb003965d
Jiri Daněk <jdanek@redhat.com>
2024-01-20 22:20:04 +0100

    Add initial script that installs crc and starts okd (#1)

---
+++ crc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -Eeuxo pipefail
+
+curl -L https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.31.0/crc-linux-amd64.tar.xz | tar -C /usr/local/bin --strip-components=1 -xJvf -
+crc config set consent-telemetry yes
+crc config set preset okd
+crc config set network-mode user
+crc config set host-network-access true
+crc setup
+crc start
+eval "$(crc oc-env)"
+oc config use-context crc-admin
+
+oc wait --for=condition=Ready nodes --all
+oc wait --for=condition=Available deployments --all --all-namespaces
+oc wait --for=condition=Ready pods --all --all-namespaces
+
+echo "DONE"
