cef67e717e55da8c344c57827ac7c2be1e98b1d9
Jiri Daněk <jdanek@redhat.com>
2024-01-20 23:16:33 +0100

    Run and test

--- crc.sh
+++ crc.sh
@@ -5,6 +5,7 @@ crc config set consent-telemetry yes
 crc config set preset openshift
 crc config set network-mode user
 crc config set host-network-access true
+crc delete
 crc setup
 crc start --disable-update-check --pull-secret-file pull-secret.txt
 eval "$(crc oc-env)"
