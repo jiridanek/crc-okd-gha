67be18e5b5aa3504bb142fe7404afd9b79e7f313
Jiri Daněk <jdanek@redhat.com>
2024-01-21 00:32:05 +0100

    need to cache whole machines/

--- crc.sh
+++ crc.sh
@@ -5,7 +5,7 @@ crc config set consent-telemetry yes
 crc config set preset okd
 crc config set network-mode user
 crc config set host-network-access true
-crc delete || true
+crc delete --clear-cache || true
 crc setup
 crc start --disable-update-check
 eval "$(crc oc-env)"
