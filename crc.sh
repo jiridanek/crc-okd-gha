579d323b3f7d8ffb63047853f2164e80c8ade7f6
Jiri Daněk <jdanek@redhat.com>
2024-01-20 21:20:51 +0100

    Add initial script that installs crc and starts okd

--- crc.sh
+++ crc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+sudo apt-get update
 sudo apt install qemu-kvm libvirt-daemon libvirt-daemon-system network-manager
 
 curl -L https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.31.0/crc-linux-amd64.tar.xz | tar -C /usr/local/bin --strip-components=1 -xJvf -
