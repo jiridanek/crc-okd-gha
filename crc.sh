b802567414a04b527ebcd44be3ddfb23e4241167
Jiri Daněk <jdanek@redhat.com>
2024-01-20 21:45:19 +0100

    Add initial script that installs crc and starts okd

--- crc.sh
+++ crc.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
-sudo apt-get update
-sudo apt install qemu-kvm libvirt-daemon libvirt-daemon-system network-manager
-sudo usermod -a -G libvirt $USER
-newgrp libvirt
-
 curl -L https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.31.0/crc-linux-amd64.tar.xz | tar -C /usr/local/bin --strip-components=1 -xJvf -
 crc config set consent-telemetry yes
 crc config set preset okd
-crc config set skip-check-root-user true
-crc config set skip-check-user-in-libvirt-group true
 crc setup
 crc start
 eval "$(crc oc-env)"
