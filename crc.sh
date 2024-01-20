f996c2b83556815f2b0e9b20b9d3b7822f750371
Jiri Daněk <jdanek@redhat.com>
2024-01-20 22:39:56 +0100

    Switch to OpenShift (with pull-secret) (#2)
    OKD has a strange 8 mins delay waiting for new certificate.

--- crc.sh
+++ crc.sh
@@ -3,11 +3,11 @@ set -Eeuxo pipefail
 
 curl -L https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.31.0/crc-linux-amd64.tar.xz | tar -C /usr/local/bin --strip-components=1 -xJvf -
 crc config set consent-telemetry yes
-crc config set preset okd
+crc config set preset openshift
 crc config set network-mode user
 crc config set host-network-access true
 crc setup
-crc start
+crc start --disable-update-check --pull-secret-file pull-secret.txt
 eval "$(crc oc-env)"
 oc config use-context crc-admin
 
