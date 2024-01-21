89db44d6781df7f812162ddf2e973aa45ec67eee
Jiri Daněk <jdanek@redhat.com>
2024-01-21 12:29:02 +0100

    need full setup

--- test.sh
+++ test.sh
@@ -5,6 +5,11 @@ crc setup
 
 #sudo virsh net-define ~/crc_net.xml
 sudo virsh define ~/crc.xml
+chmod o+x ~/
+chmod o+x ~/.crc
+chmod o+x ~/.crc/cache
+chmod o+x ~/.crc/machines/
+chmod o+x ~/.crc/machines/crc
 sudo chown libvirt-qemu:libvirt ~/.crc/machines/crc/crc.qcow2
 sudo virsh start crc
 
