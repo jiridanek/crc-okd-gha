983c28d2fd8bec4d2d0b87550479d5a14c6f0c95
Jiri Daněk <jdanek@redhat.com>
2024-01-21 12:14:32 +0100

    need full setup

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ crc setup
 
 #sudo virsh net-define ~/crc_net.xml
 sudo virsh define ~/crc.xml
-sudo chmod o+rw ~/.crc/machines/crc/crc.qcow2
+sudo chmod ugo+rw ~/.crc/machines/crc/crc.qcow2
 sudo virsh start crc
 
 eval "$(crc oc-env)"
